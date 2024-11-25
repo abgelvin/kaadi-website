8fe4392f15a814cfc7b85d4e0c5391b6bf1965a4
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-11-25 16:46:44 -0700

    added re_path to urls

--- kaadi_website/urls.py
+++ kaadi_website/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing_page.urls')),
+    re_path('media/uploads', serve, {
+        'document_url': settings.MEDIA_ROOT,
+    }),
 ]
 
 if settings.DEBUG:
