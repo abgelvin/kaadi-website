ce4e884bb91b4ff962405dc5fb2471aa64211cb4
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-11-24 18:43:56 -0700

    added / to media url

--- kaadi_website/settings.py
+++ kaadi_website/settings.py
@@ -148,7 +148,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/uploads'
+MEDIA_URL = '/media/uploads/'
 MEDIA_ROOT = '/media/uploads/'
 # MEDIA_ROOT = BASE_DIR / 'media' / 'uploads'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
