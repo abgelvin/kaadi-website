48b9722d33f410fdc897c6a6220c429eef5ee09b
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-11-26 15:43:28 -0700

    moved whitenoise middleware to second in list

--- kaadi_website/urls.py
+++ kaadi_website/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('', include('landing_page.urls')),
 ]
 
-if not settings.DEBUG:
+if  settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
