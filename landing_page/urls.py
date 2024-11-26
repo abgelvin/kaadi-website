9d107b95cee2063e90cc91183a865d08ee8acfbd
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-11-26 14:54:43 -0700

    added static to urls in landing_page

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 from .views import SuccessView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='home'), 
@@ -11,3 +14,7 @@ urlpatterns = [
     path('contact/', views.contact_view, name='contact'),
     path('success/', SuccessView.as_view(), name='success')
 ]
+
+
+if settings.DEBUG or settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
