b3fe45d481461b77f907af475d8348de3e1bc0fd
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-11-26 15:08:07 -0700

    cleared static setting from app urls.py and added to prpoject urls.py

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 from .views import SuccessView
 from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -12,9 +11,8 @@ urlpatterns = [
     # path('testimonials/', views.testimonials, name='testimonials'),
     # path('contact/', ContactView.as_view(), name='contact'), 
     path('contact/', views.contact_view, name='contact'),
-    path('success/', SuccessView.as_view(), name='success')
+    path('success/', SuccessView.as_view(), name='success'),
+    
 ]
 
 
-if settings.DEBUG or settings.MEDIA_ROOT:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
